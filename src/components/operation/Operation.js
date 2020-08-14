c13e350ec735565dce3038aadc34fa9a0ced5ce9
Alexander Petriga <cazhqa666@gmail.com>
2020-08-14 19:18:15 +0300

    update app.js/add components

---
+++ src/components/operation/Operation.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+const Operation = ({ addTransaction, addDescription, addAmount, description, amount }) => {
+    return(
+    <section className="operation">
+      <h3>Новая операция</h3>
+      <form id="form">
+          <label>
+              <input 
+                  type="text" 
+                  className="operation__fields operation__name" 
+                  placeholder="Наименование операции" 
+                  value={description}
+                  onChange={addDescription}
+              />
+          </label>
+          <label>
+                <input 
+                  type="number" 
+                  className="operation__fields operation__amount" 
+                  placeholder="Введите сумму"
+                  value={amount}
+                  onChange={addAmount}
+                />
+          </label>
+          <div className="operation__btns">
+              <button 
+                  onClick={() => addTransaction(false)} 
+                  type="button"
+                  className="operation__btn operation__btn-subtract">РАСХОД</button>
+              <button 
+                  onClick={() => addTransaction(true)}
+                  type="button" 
+                  className="operation__btn operation__btn-add"
+                  >ДОХОД</button>
+          </div>
+      </form>
+    </section>
+    )
+};
+
+export default Operation;
